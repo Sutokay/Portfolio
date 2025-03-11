40ba2f1d416c1ba2ceeeceb9216bcc420e260f83
Sutokay <mathias.ulf1@gmail.com>
2025-03-11 21:02:31 +0100

    git add requirements.txt git commit -m "Added Flask and Gunicorn to requirements" git push origin main

--- server.py
+++ server.py
@@ -92,3 +92,7 @@ if __name__ == "__main__":
     # Make sure the files directory exists
     os.makedirs(os.path.join(app.static_folder, 'files'), exist_ok=True)
     app.run(debug=True)
+
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host='0.0.0.0', port=port)
